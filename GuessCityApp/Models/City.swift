d7c0108244cc74195b8342de302b5320a03fa472
Artem Pavlov <pavlov.tema@gmail.com>
2021-09-12 23:04:05 +0500

    struct City created

--- GuessCityApp/Models/City.swift
+++ GuessCityApp/Models/City.swift
@@ -2,8 +2,22 @@
 //  City.swift
 //  GuessCityApp
 //
-//  Created by iMac on 12.09.2021.
+//  Created by Pavlov Artem on 12.09.2021
 //
 
 import Foundation
 
+struct City {
+    let titleOfCity: String
+    
+    static func getCities(amountOfQuestions: Int) -> [City] {
+        for amountOfQuestions in 1...10 {
+            
+        }
+
+    
+    }
+}
+
+
+    
