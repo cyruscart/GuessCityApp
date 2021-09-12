a6824059c5d5f342658185e9546d603eb3108a5f
Kirill Telegin <mr.tks@yandex.ru>
2021-09-12 18:23:51 +0600

    Сделана рабочая структура главного экрана с имитацией входных данных.

---
+++ GuessCityApp/Models/City.swift
@@ -0,0 +1,35 @@
+//
+//  City.swift
+//  GuessCityApp
+//
+//  Created by Кирилл on 11.09.2021.
+//
+
+import Foundation
+
+class City {
+    var name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+    
+}
+
+let citiesList = [City(name: "Москва"),
+               City(name: "Петербург")]
+
+let listOfCities = ["Майкоп",
+                    "Москва",
+                    "Петербург",
+                    "Ярославль",
+                    "Киров",
+                    "Воронеж",
+                    "Казань",
+                    "Самара",
+                    "Чебоксары",
+                    "Омск",
+                    "Сочи",
+                    "Брянск"]
+
+
